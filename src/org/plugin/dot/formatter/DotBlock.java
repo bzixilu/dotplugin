7f1e97479ec22900e6037baaa502498237a4681f
Olga <bzixilu@gmail.com>
2017-02-07 20:24:44 +0300

    ; was missing

--- src/org/plugin/dot/formatter/DotBlock.java
+++ src/org/plugin/dot/formatter/DotBlock.java
@@ -64,7 +64,7 @@ public class DotBlock extends AbstractBlock {
             if(myNode.getElementType().equals(DotTypes.CURLY_BRACKET_RIGHT)){
                 return Indent.getNoneIndent();
             }
-            return Indent.getSpaceIndent(4)
+            return Indent.getSpaceIndent(4);
         }
         // TODO: comment are not intended
         return Indent.getNoneIndent();
