6de53d6f7309b7218132bb326ec963701ccec5c5
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2023-10-26 16:16:32 +0200

    switch to gradle
    switch to gradle
    
    switch to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("java-library")
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("org.jetbrains.intellij") version "1.16.0"
+}
+
+group = "org.plugin.dot"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Include the generated files in the source set
+sourceSets {
+    main {
+        java {
+            srcDirs("src/main/gen")
+        }
+    }
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2021.3.3")
+    type.set("IC")
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+dependencies {
+    api("guru.nidi:graphviz-java:0.18.1") {
+        exclude("org.slf4j", "slf4j-api")
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+
+
+    patchPluginXml {
+        sinceBuild.set("213")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
--- src/main/java/org/plugin/dot/DotIcons.java
+++ src/main/java/org/plugin/dot/DotIcons.java
@@ -8,7 +8,7 @@ import javax.swing.*;
  * Class stores path to icon file using for dot language visualization
  */
 public class DotIcons {
-    public static final Icon FILE = IconLoader.getIcon("/org/plugin/dot/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("favicon.png");
 
-    public static final Icon SAVE = IconLoader.getIcon("/org/plugin/dot/icons/save.png");
+    public static final Icon SAVE = IconLoader.getIcon("save.png");
 }
